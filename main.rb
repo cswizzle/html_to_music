1d7cffce6b5a5feef976eb950d1c0ae08099a68c
Clarinda <clarinda1017@gmail.com>
2017-01-21 17:30:26 -0500

    prints current tag

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ tags = Parser.new(ARGV[0]).parse
 notes = tags.uniq.each_with_index.map{|tag, index| {tag => "./sounds/#{index + 1}.wav"}}.reduce({}, :merge)
 
 tags.each do |tag|
+	print "#{tag} "
 	MPlayer::Slave.new(notes[tag])
 	sleep 0.1
 end
